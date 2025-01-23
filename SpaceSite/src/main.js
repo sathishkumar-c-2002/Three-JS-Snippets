c622a20dac968f9936fea89fb791b3bd6522af3d
csathishkumar <csathishkumar2002@gmail.com>
2025-01-23 22:48:00 +0530

    SpaceSite - Created Control for GridHelper - Version 2

--- SpaceSite/src/main.js
+++ SpaceSite/src/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
@@ -12,10 +13,23 @@ camera.position.setZ(30);
 // renderer.render(scene,camera); 
 
 const geometry = new THREE.TorusGeometry(10,3,16,100);
-const material = new THREE.MeshBasicMaterial({color:0xff6347,wireframe:true});
+const material = new THREE.MeshStandardMaterial({color:0xff6347});
 const torus = new THREE.Mesh(geometry,material);
 
 scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(10, 10, 10); 
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight,ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200,50)
+scene.add(lightHelper,gridHelper);
+
+const controls = new OrbitControls(camera,renderer.domElement);
+
 
 function animate(){
   requestAnimationFrame(animate);
@@ -23,7 +37,11 @@ function animate(){
   torus.rotation.y += 0.01;
   torus.rotation.z += 0.01;
 
+  controls.update();
+
   renderer.render(scene,camera)
 }
+
+
 
 animate();
