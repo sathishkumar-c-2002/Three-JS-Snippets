5123ba1f7696774e725ba06a7d5ab14192b1c319
csathishkumar <csathishkumar2002@gmail.com>
2025-05-01 15:13:56 +0530

    changed model

--- Honey Bee/src/main.js
+++ Honey Bee/src/main.js
@@ -15,7 +15,7 @@ let bee;
 let mixer;
 
 const loader = new GLTFLoader();
-loader.load('/demon_bee_full_texture.glb',
+loader.load('/test.glb',
      function(gltf){
         bee = gltf.scene;   
         // bee.position.x = 0;
